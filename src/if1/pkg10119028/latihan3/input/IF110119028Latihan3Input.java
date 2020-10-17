3ce29d26bd30dd695e25721fb3b7b3168fb4fbfd
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:05:44 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan3/input/IF110119028Latihan3Input.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan3.input;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk
+ */
+public class IF110119028Latihan3Input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+    }
+    
+}
